7f92812e06bf92709279632d2881e1c0bcdd2cfb
can us <66898420+canxue32@users.noreply.github.com>
2023-07-19 21:13:50 +0800

    Update caj.js

--- caj.js
+++ caj.js
@@ -1,38 +1,38 @@
 /*
 
-Zoomable：https://apps.apple.com/app/id1568442831
+HashPhotos：https://apps.apple.com/app/id685784609
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/Zoomable.js
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/Guding88/Script/main/Zoomable.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/HashPhotos.js
 
 [MITM]
-hostname = api.revenuecat.com
+hostname = buy.itunes.apple.com
 
 */
-const guding = {};
-const guding6 = JSON.parse(typeof $response != "undefined" && $response.body || null);
-if (typeof $response == "undefined") {
-  delete $request.headers["x-revenuecat-etag"];
-  delete $request.headers["X-RevenueCat-ETag"];
-  guding.headers = $request.headers;
-} else if (guding6 && guding6.subscriber) {
-  guding6.subscriber.subscriptions = guding6.subscriber.subscriptions || {};
-  guding6.subscriber.entitlement = guding6.subscriber.entitlement || {};
-  const app = 'gd';const list = {'gd':{name: 'vip', id: 'com.shengzhou.fileartifact.year'}};
-  const data = {
-    "expires_date": "6666-06-06T06:06:06Z",
-    "original_purchase_date": "2023-02-23T02:33:33Z",
-    "purchase_date": "2023-02-23T02:33:33Z",
-    "ownership_type" : "PURCHASED",
-    "store" : "app_store"};
-for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
-guding6.subscriber.subscriptions[list[i].id] = data;
-guding6.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
-guding6.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
-                break;
-          }
+var guding = JSON.parse($response.body);
+guding = {
+  "environment": "Production",
+  "pending_renewal_info": [
+    {
+      "product_id": "com.shengzhou.fileartifact.year",
+      "auto_renew_status": "1"
     }
-    guding.body = JSON.stringify(guding6);
+  ],
+  "status": 0,
+  "latest_receipt_info": [
+    {
+      "quantity": "1",
+      "purchase_date_ms": "1688386973000",
+      "expires_date": "6666-06-06 06:06:06 Etc\/GMT",
+      "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles",
+      "is_in_intro_offer_period": "false",
+      "transaction_id": "540001260447637",
+      "is_trial_period": "false",
+      "original_transaction_id": "540001260447637",
+      "product_id": "com.shengzhou.fileartifact.year",
+      "original_purchase_date_ms": "1688386974000",
+      "expires_date_ms": "148204937166000"
+    }
+  ]
 }
-$done(guding);
+$done({ body: JSON.stringify(guding) });
