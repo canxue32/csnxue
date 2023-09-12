de505e86a62ce9ff5a0cdec5519101196606d162
can us <66898420+canxue32@users.noreply.github.com>
2023-09-12 11:00:31 +0800

    Update caj.js

--- caj.js
+++ caj.js
@@ -1,38 +1,12 @@
-/*
-
-HashPhotos：https://apps.apple.com/app/id685784609
-
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/HashPhotos.js
+^https?:\/\/www\.guga\.co\/api-base\/v1\/state|https?:\/\/www\.guga\.co\/api-base\/v1\/preferential-product|https?:\/\/*\.guga\.co.*?.*? url script-response-body guga.js
 
-[MITM]
-hostname = buy.itunes.apple.com
 
-*/
-var guding = JSON.parse($response.body);
-guding = {
-  "environment": "Production",
-  "pending_renewal_info": [
-    {
-      "product_id": "top.freshhome.outdue.forever",
-      "auto_renew_status": "1"
-    }
-  ],
-  "status": 0,
-  "latest_receipt_info": [
-    {
-      "quantity": "1",
-      "purchase_date_ms": "1688386973000",
-      "expires_date": "6666-06-06 06:06:06 Etc\/GMT",
-      "expires_date_pst": "6666-06-06 06:06:06 America\/Los_Angeles",
-      "is_in_intro_offer_period": "false",
-      "transaction_id": "540001260447637",
-      "is_trial_period": "false",
-      "original_transaction_id": "540001260447637",
-      "product_id": "top.freshhome.outdue.forever",
-      "original_purchase_date_ms": "1688386974000",
-      "expires_date_ms": "148204937166000"
-    }
-  ]
-}
-$done({ body: JSON.stringify(guding) });
+[mitm]
+hostname = www.guga.co,www.guga.co,*.guga.co
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/"level\":\d/g,'"level":1');
+body = body.replace(/"expire\":\d/g,'"expire":1');
+$done(body);
