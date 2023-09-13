61240ea540fb5f1050b21922875e476f23ebf484
can us <66898420+canxue32@users.noreply.github.com>
2023-09-13 10:48:52 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/Lister.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/canxue32/csnxue/main/2.js
 
 [mitm]
 hostname = buy.itunes.apple.com
