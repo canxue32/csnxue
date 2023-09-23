bc7500c06a2f060a171efccf0d0d887ef0e5be3b
can us <66898420+canxue32@users.noreply.github.com>
2023-09-23 19:31:41 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -13,7 +13,7 @@ hostname = api.revenuecat.com
 const chxm1023 = {};
 const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
-const name = "premium";
+const name = "Premium";
 const appid = "radioapp.lifetime";
 
   
