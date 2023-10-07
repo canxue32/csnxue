04c749d2f1ef5a60138b4881d014b4ab89c82c26
can us <66898420+canxue32@users.noreply.github.com>
2023-10-07 15:57:19 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -13,7 +13,7 @@ hostname = api.revenuecat.com
 const chxm1023 = {};
 const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 
-const name = "Pro";
+const name = "pro";
 const appid = "igrammar_199_1y_v1";
 
   
