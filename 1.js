8975e1f329179a19e3ff4a80280256c9d3c3e274
can us <66898420+canxue32@users.noreply.github.com>
2024-07-28 14:53:59 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -13,7 +13,7 @@ hostname = api.revenuecat.com
 const chxm1023 = {};
 const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 const name = "pro";
-const appid = "igrammar_199_1y_v1";
+const appid = "soma_t_yearly";
 
   
 if (typeof $response == "undefined") {
